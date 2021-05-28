674255d122bae1a6726c0dac5244b23ad365b03b
Filipe <filipesluiz@gmail.com>
2021-05-28 15:36:26 -0300

    criado novo projeto users (client)

---
+++ quarkus-users-service/src/test/java/org/sample/users/NativeUsersResourceIT.java
@@ -0,0 +1,9 @@
+package org.sample.users;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeUsersResourceIT extends UsersResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ quarkus-users-service/src/test/java/org/sample/users/UsersResourceTest.java
@@ -0,0 +1,21 @@
+package org.sample.users;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class UsersResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/check")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+}
