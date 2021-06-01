44cf36984814e5bcf3e46cd6bae47f66935ac003
Filipe <filipesluiz@gmail.com>
2021-06-01 16:01:46 -0300

    remove import

--- quarkus-product-service/src/main/java/org/sample/client/UserService.java
+++ quarkus-product-service/src/main/java/org/sample/client/UserService.java
@@ -4,7 +4,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
